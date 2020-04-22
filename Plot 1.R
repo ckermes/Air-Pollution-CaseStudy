7e6e73b9d0ef7ae8dfbee2e52b8dbe496a78f45c
ckermes <ckermes2@gmail.com>
2020-04-22 12:22:39 -0400

    united states graph vs baltimore in header

--- Plot 1.R
+++ Plot 1.R
@@ -9,7 +9,7 @@ sum_by_year <- aggregate(NEI$Emissions, by=list(year=NEI$year), FUN=sum)
 #plot total emissions for all four years
 png(filename = "plot1.png")
 plot(sum_by_year$year, sum_by_year$x, type = "l",
-     main = "Total Emissions of PM2.5 in Baltimore",
+     main = "Total Emissions of PM2.5 in the United States",
      xlab = "Year",
      ylab = "Total Emissions of PM2.5 (tons)")
 dev.off()
